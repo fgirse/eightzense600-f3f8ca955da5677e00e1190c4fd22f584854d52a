976a99dd69502e631d077e1f162e22f7a825454e
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-12-11 04:27:44 +0100

    41er commmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,50 +29,67 @@ const Home = () => {
              <SeoMeta />
       {/* ============================================== GRID A ====================================*/}
  
- <main className=" h-screen w-full  bg-[url('/images/grid.svg')]  bg-reāeat  bg-[#3c3f40] mb-10 grid grid-cols-[1fr_1fr_1fr] grid-rows-[85vh_20vh_20vh_50aaaavh] md:grid-cols-5 md:grid-rows-[89vh_20vh_45vh_60vh_50vh] lg:grid-cols-8 lg:grid-rows-[25vh_60vh_45vh_25vh_50vh] xl:grid-cols-12 xl:grid-row-[25vh_66vh_30vh_25vh_25vh] gap-3">
- <section className="col-start-1 col-end-4 row-start-1 row-end-2 md:col-start-1 md:col-end-4  md:row-start-1 md:row-end-2  md:h-auto lg:w-full lg:col-start-1 lg:col-end-13">
-      <div className="bg-[url('/images/sideboardobg.png')] bg-contain bg-no-repeat bg-bottom grid gap-y-1 grid-cols-[1fr_1fr_1fr] grid-rows-[34vh_18vh_4vh_10vh] md:grid-cols-[26vw_20vw_20vw_20vw_20vw] md:grid-rows-[24vw_38vh_33vh_20vh]">
-          <div className="p-3 col-start-1 col-end-4 5row-start-1 row-end-2 bg-gradient-to-b to-[#3c3f40] md:bg-gradient-to-b from-blue-600/30 md:col-start-1 md:col-end-6 md:row-start-1 md:row-end-2">
-          <p className=" py-2 px-2 text-5xl  md:mb-10 md:w-8/12 md:py-6 md:text-[5rem] leading-9 font-bold text-stone-100 lg:px-7 lg:mt-7 lg:leading-77 lg:text-[5rem]">Timeless</p>
-          <p className="px-2 leading-10 text-8xl  md:text-[9rem] font-extrabold text-[#d8cdab] lg:text-[13rem] mt-3">Design</p>
-          </div>
-
-          <div className="relative z-10 col-start-2 col-end-4 row-start-1 row-end-2 py-28 leading-3 font-bold text-[.95rem] mt-12 rounded-md w-11/12  font-primary text-stone-400  text-right md:col-start-4 md:col-end-6 md:row-start-1 md:row-end-2 mr:24 md:text-[1.523rem] md:leading-5 lg:text">
-                  <p className=" border-r-8  border-amber-600 text-stone-300 lg:mt-12"
+      <div className="grid grid-cols-12 grid-rows-18 gap-x-1 ">
+    <div className="col-span-8 row-span-2">
+    <p className="uppercase py-2 px-2 text-4xl  md:mb-10 md:w-8/12 md:py-6 md:text-[5rem] leading-1 font-bold text-stone-100 lg:px-7 lg:mt-7 lg:leading-77 lg:text-[5rem]">Timeless</p>
+    </div>
+    <div className="col-span-8 row-span-4 col-start-1 row-start-3">
+    <p className="relative -top-6 uppercase px-2 text-6xl font-black  md:text-[9rem] text-[#d8cdab] lg:text-[13rem] mt-3">Design</p>
+    </div>
+    <div className="col-span-4 row-span-6 col-start-9 row-start-1"><p className="py-3 text-[.5rem] leading-1 text-right border-r-8  border-amber-600 text-stone-300 lg:mt-12"
                           dangerouslySetInnerHTML={markdownify(banner.content ?? "")}/>
-                   <p className=" mt-2 font-phudu lg:mt-3 lg:ml-5 text-[.59rem] w-8 h-8 bg-slate-900/60 leading-3 shadow-xl shadow-gray-500 rounded-full text-center font-medium font-phudu text-orange-500 md:w-12 lg:w-24 lg:text-xs">carl sagan</p>
-           </div>
+                   <p className="relative left-20 py-1 font-phudu   lg:ml-5 text-[.59rem] w-9 h-8 bg-orange-500 leading-3 shadow-xl shadow-gray-500 rounded-full text-center font-medium font-phudu text-white md:w-12 lg:w-24 lg:text-xs">Carl<br/>Sagan</p>
+            </div>
+    <div className="col-span-12 row-span-4 row-start-7">
+    <div className=" col-start-2 col-end-3 row-start-3 row-end-4 md:w-[66v] md:h-[50vh]">
+           <Image
+              src="/images/sideboardobg.png"
 
-     
-        
+              height="550"
+              width="550" 
+              alt="Sideboard"
+              className="">
 
-      <div className=" flex justify-center col-start-2 col-end-3 row-start-4 row-end-5 md:col-start-2 md:col-end-4 md:row-start-4 md-row-end-5 md:text-[3rem] " >
+              </Image>
+              </div>
+    </div>
+      
+    </div>
+    <div className="col-span-5 row-span-5 col-start-4 row-start-11">
+    <div className="">
+           <Image
+              src="/images/LogoEZ.png"
+
+              height="166"
+              width="166" 
+              alt="Logo"
+              className="mb-5 mx-auto rounded-xl shadow-xl shadow-white/50 ">
+
+              </Image>
+              </div>
+    </div>
+    <div className="col-span-4 row-span-2 col-start-5 row-start-16"> <div className=" flex justify-center col-start-2 col-end-3 row-start-2 row-end-3 md:col-start-2 md:col-end-4 md:row-start-2 md-row-end-3 md:text-[3rem] " >
 {banner.button!.enable && (
-          <a className="mt-5 bg-amber-500 rounded-xl btn btn-primary text-center w-36 h-10 text-1xl md:text-2xl" href={banner.button!.link}>
+          <a className=" bg-amber-500 rounded-xl btn btn-primary text-center w-36 h-10 text-1xl md:text-2xl" href={banner.button!.link}>
             {banner.button!.label}
           </a>
         )}
   </div>
-  
-       
-           <Image
-              src="/images/LogoEZ.png"
+  </div>
 
-              height="250"
-              width="250" 
-              alt="Logo"
-              className="mt-32 w-28 mx-auto rounded-xl shadow-xl shadow-white/50 col-start-2 cols-end-3 row-start-4 row-end-5">
 
-              </Image>
-  
 
-    
-    </div>
-</section>
-</main>
+
+
+
+
+
+
+
+
 
     {/* ==============================================  ENDE GRID A ====================================*/}
-<section className="">
+<section className="py-7">
 <Gallery />
 </section>
      
